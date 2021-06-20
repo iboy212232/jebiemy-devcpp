319e62357c97c741fc559bb4eefd3d169be1b4ad
iboy212232 <macieklen360@gmail.com>
2021-06-20 23:05:44 +0200

    update wykon
    add more messy code

--- wykon.cpp
+++ wykon.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <dos.h>
+#include <windows.h>
  using namespace std;
  float a;
+ int t;
  int main ()
  {
  	
  	cout<<"Welcome to me project"<<endl;
  	cout<<"try to have fun"<<endl;
- 	
+ 	sleep (3);
+ 	system("CLS");
+ 	cout<<"give me a number :\n";
+ 	cin>>a;
+ 	cout<<"your number is: "<<a<<"  "<<endl;
+ 	system("CLS");
+ 	cout<<"are you sure? "<<endl;
+ 	cout<<"1. true"<<endl;
+ 	cout<<"2. false"<<endl;
+ 	cin>>t;
  	
  	
 return 0;
